7157441ebe2acd212dcfe028c69bbf665f16f086
NoritomoTaba <minamiedamame0328@gmail.com>
2019-05-30 12:38:54 +0900

    modified: .zplugのためのexport設定

--- .zsh/export.zsh
+++ .zsh/export.zsh
@@ -30,3 +30,5 @@ export PATH=$PATH:$GOPATH/bin
 export LSCOLORS=cxfxcxdxbxegedabagacad
 export LS_COLORS='di=34:ln=35:so=32:pi=33:ex=31:bd=46;34:cd=43;34:su=41;30:sg=46;30:tw=42;30:ow=43;30'
 
+# zplug (zsh plugin manager)
+export ZPLUG_HOME=~/.zplug
