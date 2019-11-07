14e75815cb2d2ea1d34fda360dd0e1fc2711f712
cat2koban <10192563+cat2koban@users.noreply.github.com>
2019-11-07 18:51:01 +0900

    update .zsh/

--- .zsh/plugin.zsh
+++ .zsh/plugin.zsh
@@ -4,6 +4,8 @@ source ~/.zplug/init.zsh
 # plugin for zsh like fish
 zplug "zsh-users/zsh-syntax-highlighting", defer:2
 zplug "zsh-users/zsh-autosuggestions", defer:2  
+zplug mafredri/zsh-async, from:github
+zplug sindresorhus/pure, use:pure.zsh, from:github, as:theme
 
 # Install plugins if there are plugins that have not been installed
 if ! zplug check --verbose; then
