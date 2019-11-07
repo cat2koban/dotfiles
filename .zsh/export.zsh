5d61b7800557fd5f6ad1adcfa6c156008eed0e3c
cat2koban <10192563+cat2koban@users.noreply.github.com>
2019-11-07 18:28:22 +0900

    update something...

--- .zsh/export.zsh
+++ .zsh/export.zsh
@@ -4,6 +4,10 @@ export LANG=ja_JP.UTF-8
 # PATH
 export PATH="/usr/local/bin:$PATH"
 
+# rbenv
+export PATH="$HOME/.rbenv/bin:$PATH"
+eval "$(rbenv init -)"
+
 # cmd [ls] color settings 
 export LSCOLORS=cxfxcxdxbxegedabagacad
 export LS_COLORS='di=34:ln=35:so=32:pi=33:ex=31:bd=46;34:cd=43;34:su=41;30:sg=46;30:tw=42;30:ow=43;30'
