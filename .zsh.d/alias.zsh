ef4d0e00e2e743405da87a8afab9dbd3ecd97bb1
Noritomo Taba <minamiedamame0328@gmail.com>
2020-04-09 00:54:39 +0900

    ghq | peco | hub を入れてからね

--- .zsh.d/alias.zsh
+++ .zsh.d/alias.zsh
@@ -12,3 +12,7 @@ alias gic='git commit'
 alias gif='git diff'
 alias gilo='git log --oneline'
 alias gilog='git log --oneline --graph'
+
+# ghq | peco | hub 
+alias g='cd $(ghq root)/$(ghq list | peco)'
+alias gh='hub browse $(ghq list | peco | cut -d "/" -f 2,3)'
