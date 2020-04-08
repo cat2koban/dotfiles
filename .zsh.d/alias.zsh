8b85417d293479caca161dd706df0b43e4314d48
Noritomo Taba <minamiedamame0328@gmail.com>
2020-04-08 23:09:55 +0900

    .zsh/pure とかぶると怖いなあと思って変更
    TODO: 結局 $HOME に .zsh と .zsh.d が並ぶの、
    どうなんだろうなぁっていう懸念がある。

---
+++ .zsh.d/alias.zsh
@@ -0,0 +1,14 @@
+alias rm='rm -i'
+alias ls='ls -aG -F'
+#alias vi='/usr/local/bin/vim'
+#alias vim='/usr/local/bin/vim'
+
+# cd
+alias dot='cd $HOME/workspace/dotfiles'
+
+# git
+alias gis='git status'
+alias gic='git commit'
+alias gif='git diff'
+alias gilo='git log --oneline'
+alias gilog='git log --oneline --graph'
---
+++ .zsh.d/basic.zsh
@@ -0,0 +1,9 @@
+HISTFILE=~/.zsh_history
+HISTSIZE=1000000
+SAVEHIST=1000000
+
+autoload -Uz colors; colors
+
+autoload -U compinit; compinit
+
+bindkey -e
