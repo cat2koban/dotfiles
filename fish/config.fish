7e3ae62d48315f9f204cb9d239878587a871dafb
Noritomo Taba <cat2koban@users.noreply.github.com>
2020-04-18 19:13:36 +0900

    Modified configuration alias

--- fish/config.fish
+++ fish/config.fish
@@ -4,10 +4,11 @@ alias rm='rm -i'
 alias gis='git status'
 alias vi='nvim'
 alias vim='nvim'
-alias vimconf='vi ~/.config/nvim/init.vim'
-alias fishconf='vi ~/.config/fish/config.fish'
+alias vimconf='vi ~/ghq/github.com/cat2koban/dotfiles/nvim/init.vim'
+alias fishconf='vi ~/ghq/github.com/cat2koban/dotfiles/fish/config.fish'
 alias reload='exec $SHELL'
 alias dot='cd ~/ghq/github.com/cat2koban/dotfiles/'
+alias get='ghq get -p'
 
 # fish-ghq / fzf 
 set -g GHQ_SELECTOR_OPTS "--no-sort --reverse --ansi --color bg+:13,hl:3,pointer:7"
