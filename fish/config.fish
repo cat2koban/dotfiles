676cf8d2c2a25be3a95d9a42584f0e557745cf50
Noritomo Taba <cat2koban@users.noreply.github.com>
2020-05-09 17:51:40 +0900

    added explanation line & alias for rails-dev

--- fish/config.fish
+++ fish/config.fish
@@ -1,17 +1,43 @@
 # alias
+## regular commands
 alias ls='ls -aG -F'
 alias rm='rm -i'
+
+## git
 alias gis='git status'
+alias gif='git diff'
+
+## nvim
 alias vi='nvim'
 alias vim='nvim'
+
+## configuration
 alias vimconf='vi ~/ghq/github.com/cat2koban/dotfiles/nvim/init.vim'
 alias fishconf='vi ~/ghq/github.com/cat2koban/dotfiles/fish/config.fish'
-alias reload='exec $SHELL'
+
+## dotfiles
 alias dot='cd ~/ghq/github.com/cat2koban/dotfiles/'
+
+## ghq
 alias get='ghq get -p'
+
+## others
+alias reload='exec $SHELL'
+
+# for Development rails app
+alias be='bundle exec'
+alias bers='be rspec'
+alias berc='be rubocop'
+alias berca='be rubocop -a'
 
 # fish-ghq / fzf 
 set -g GHQ_SELECTOR_OPTS "--no-sort --reverse --ansi --color bg+:13,hl:3,pointer:7"
+
+# direnv
+eval (direnv hook fish)
+
+# editor
+set -x EDITOR nvim
 
 # GPG Key
 set -x GPG_TTY (tty)
