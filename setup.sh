e0ccd0d085ab5fabd9ef4451361f5cfaf8ed63e3
cat2koban <cat2koban@users.noreply.github.com>
2021-08-01 21:29:52 +0900

    bye fish :)

--- setup.sh
+++ setup.sh
@@ -5,10 +5,8 @@ DIR=`pwd`
 # xargs brew install < $DIR/brew_list
 
 ln -vfs $DIR/nvim/init.vim $HOME/.config/nvim/init.vim
-ln -vfs $DIR/fish/config.fish $HOME/.config/fish/config.fish
-# TODO: fish を卒業したい
-#ln -vfs $DIR/zsh/.zshrc $HOME/.zshrc
-#ln -vfs $DIR/zsh/rc $HOME/
+ln -vfs $DIR/zsh/.zshrc $HOME/.zshrc
+ln -vfs $DIR/zsh/rc $HOME/
 
 ln -vfs $DIR/git/.gitconfig $HOME/.gitconfig
 ln -vfs $DIR/tmux/.tmux.conf $HOME/.tmux.conf
